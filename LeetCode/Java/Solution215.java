6c50166d189c99cf6ae9db31d8fa1ef043af3302
victor <vicebery@163.com>
2020-06-16 23:26:38 +0800

    215

---
+++ LeetCode/Java/Solution215.java
@@ -0,0 +1,27 @@
+package Java;
+
+import java.util.PriorityQueue;
+
+public class Solution215 {
+    public static void main(String[] args){
+        int[] arg = {23,45,566,7,877,1,2};
+        int k = 4;
+        System.out.println(findKthLargest(arg,k));
+    }
+
+    /**
+     * @Description: https://leetcode-cn.com/problems/kth-largest-element-in-an-array/ 数组中的第K个最大元素
+     * @Author: vicebery
+     * @Date: 2020/0616
+     **/
+    // 最小堆实现，优先队列
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>((n1, n2) -> n1 - n2);  // 默认实现为 最小堆
+        for (int n : nums) {
+            heap.add(n);
+            if (heap.size() > k)
+                heap.poll();
+        }
+        return heap.poll();
+    }
+}
