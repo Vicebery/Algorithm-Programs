948a0c6b1b9bbb176bb34e75e1f7ecaa9def148e
victor <vicebery@163.com>
2020-07-02 09:12:25 +0800

    1081

---
+++ LeetCode/Java/Solution1081.java
@@ -0,0 +1,24 @@
+package Java;
+
+import com.sun.xml.internal.bind.v2.TODO;
+
+public class Solution1081 {
+    public static void main(String[] args){
+        int[] arg = {1,2};
+        Solution1081 s = new Solution1081();
+
+        System.out.println();
+    }
+
+    /**
+     * @Description: https://leetcode-cn.com/problems/smallest-subsequence-of-distinct-characters/
+     * @Author: vicebery
+     * @Date: 2020/0702
+     **/
+    //TODO
+    public String smallestSubsequence(String text) {
+        if(text==null || text.length()==0) return "";
+        if(text.length()==1) return text;
+        return text;
+    }
+}
